f19f10a2d48ca9de967eb7ceae545a391d4819b7
alexanderamundsen <alexbolgenamundsen@gmail.com>
2025-06-18 14:24:17 +0200

    error fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ContactForm } from "@/components/ui/contact-form"
+import { ContactForm } from "@/components/ContactForm"
 import { useEffect } from "react"
 
 export default function ContactPage() {
